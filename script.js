b4030160718431d767e75870123b1f0196f5e7c0
Yuliia Hots <goculiya@gmail.com>
2024-01-07 17:30:24 +0100

    Ajout de la fonctionalité de la bouton egal

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // script.js
 window.onload = function() {
-    alert("Welcome to this webpage!");
+    const tabBtn = document.querySelectorAll(".button");
+    for (let i=0; i < tabBtn.length; i++) {
+        tabBtn[i].addEventListener("click", function (event){  
+            const monitor = document.querySelector(".monitor");
+            monitor.innerHTML =  monitor.innerHTML + event.target.dataset.id;
+        })
+    }
+    const btnEgal = document.querySelector(".btn-egal");
+    btnEgal.addEventListener("click", function (event){
+        const monitor =  document.querySelector(".monitor");
+        monitor.innerHTML = eval(monitor.innerHTML);
+        
+    })
+    
+    
+    
+    
+    
+    
+    
     // You can add more JavaScript code here
-  };
-  
+};
+
